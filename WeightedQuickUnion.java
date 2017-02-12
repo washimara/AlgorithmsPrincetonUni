de5e6693c54297f803b42b7530e907b345ef54e9
Maximus Chaus <washimara@users.noreply.github.com>
2017-02-12 22:03:06 +0000

    Create  WeightedQuickUnion.java

---
+++ WeightedQuickUnion.java
@@ -0,0 +1,48 @@
+public class WeightedQuickUnionUF
+{
+private int[] id;
+private int[] sz;
+
+public WeightedQuickUnionUF(int N) //Sets id of each object to itself. N array Access
+{
+id = new int[N];
+
+for(i=0; i<N; i++){
+
+id[i]=i;
+
+}
+
+public int root(int i){ //chase parent pointers untill root is reached. Depth if i array accesses
+
+while(i != id[i]){
+i = id[i];
+}
+
+return i;
+
+}
+
+public boolean connected(int p, int q){ //checks if p and q have the same root. Depth of p and q array accesses
+
+return root[p] == root[q];
+
+}
+
+public void WeightedUnion(int p, int q){ //Change root of p to point ot root of q. Depth of p and q array accesses.
+
+int i = root(p);
+int j = root(q);
+
+if (i == j) return;
+		if (sz[i] < sz[j]) {
+			id[i] = j;
+			sz[j] += sz[i];
+		} else {
+			id[j] = i;
+			sz[i] += sz[j];
+		}
+
+}
+
+}
